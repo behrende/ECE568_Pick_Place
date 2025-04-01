248be0117ccc380798fd9c5157dfc9b250bb0ef8
Alex Lin <alexlinys@gmail.com>
2025-03-31 22:41:21 -0700

    Continuously ask for images with test script; separates loading of model.

--- test_yolo.py
+++ test_yolo.py
@@ -11,35 +11,37 @@ model = YOLO("./models/colorcubes2.pt")
 # results = model.predict(image_path)
 # print(results)
 
-# Load an image
-image = cv2.imread("./test_images/" + args.image_path)
+while True:
+    image_path = input("Enter the image path: ")
+    # Load an image
+    image = cv2.imread("./test_images/" + image_path)
 
-# Perform inference
-results = model.predict(image, conf=0.1)
+    # Perform inference
+    results = model.predict(image, conf=0.1)
 
-# Get bounding boxes and coordinates
-for result in results:
-    for box in result.boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Convert to integers
-        conf = box.conf[0].item()  # Confidence score
-        cls = int(box.cls[0].item())  # Class ID
+    # Get bounding boxes and coordinates
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  # Convert to integers
+            conf = box.conf[0].item()  # Confidence score
+            cls = int(box.cls[0].item())  # Class ID
 
-        # Draw bounding box
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cx = (x1 + x2) / 2
-        cy = (y1 + y2) / 2
-        cls_id = int(box.cls[0].item())
-        label = model.names[cls_id]
-        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        print(f"Object: {label} X: {cx} Y: {cy} Confidence score {conf}")        
+            # Draw bounding box
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cx = (x1 + x2) / 2
+            cy = (y1 + y2) / 2
+            cls_id = int(box.cls[0].item())
+            label = model.names[cls_id]
+            cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            print(f"Object: {label} X: {cx} Y: {cy} Confidence score {conf}")        
 
-# Save and display the image with boxes
-cv2.imwrite("cube_detection.jpg", image)
-# cv2.imshow("Detection", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    # Save and display the image with boxes
+    cv2.imwrite("cube_detection.jpg", image)
+    # cv2.imshow("Detection", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-# model.train(data="coco8.yaml", epochs=3)
-# metrics = model.val()
-# model.export(format="onnx")
-# results.pandas().xyxy
+    # model.train(data="coco8.yaml", epochs=3)
+    # metrics = model.val()
+    # model.export(format="onnx")
+    # results.pandas().xyxy
